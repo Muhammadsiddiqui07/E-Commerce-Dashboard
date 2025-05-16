4211999ed69d737dfdce24376c6605d88b681dd7
Muhammad Siddiqui <muhammadsiddiqui1410@gmail.com>
2025-05-16 03:32:13 -0700

    routing-issue-solve

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Revenue_Analysis_Page from '../pages/Revenue_Analysis_Page.jsx'
 import Inventry_Management from '../pages/Inventry_Management.jsx'
 import Product_Registeration from '../pages/Product_Register.jsx'
@@ -6,15 +6,16 @@ import Product_Registeration from '../pages/Product_Register.jsx'
 
 function MyRoutes() {
     return (
-        <BrowserRouter>
+        <Router>
             <Routes>
                 <Route path='/' element={<Revenue_Analysis_Page />} />
                 <Route path='/revenu_analysis' element={<Revenue_Analysis_Page />} />
                 <Route path='/inventry_management' element={<Inventry_Management />} />
                 <Route path='/product_register' element={<Product_Registeration />} />
             </Routes>
-        </BrowserRouter>
-    )
+        </Router>
+    );
 }
+
 
 export default MyRoutes
